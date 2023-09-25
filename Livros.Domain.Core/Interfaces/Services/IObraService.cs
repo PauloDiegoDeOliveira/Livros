d380a219285dac61c8b3545c2dba74a1b40d6057
Paulo Diego <paulo.diego@unip.br>
2023-09-25 14:08:18 -0300

    Clean code

--- Livros.Domain.Core/Interfaces/Services/IObraService.cs
+++ Livros.Domain.Core/Interfaces/Services/IObraService.cs
@@ -15,9 +15,5 @@ namespace Livros.Domain.Core.Interfaces.Services
         bool ExisteNome(Obra obra);
 
         bool ExisteVolumeId(Guid id);
-
-        bool ExisteNomeVolume(Volume volume);
-
-        bool ExisteNumeroVolume(Volume volume);
     }
 }
